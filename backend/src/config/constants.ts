eaf5ea25cfbef2b2dec4ee962feeb99d9ceadcdb
Luka <lukaliuzzidev@gmail.com>
2024-07-03 06:21:39 -0300

    chore: Update API_URL constant for dynamic versioning

--- backend/src/config/constants.ts
+++ backend/src/config/constants.ts
@@ -1,3 +1,6 @@
 export const PORT = process.env.PORT || 8080
 export const DATABASE_TYPE = process.env.DATABASE_TYPE || "prisma"
 export const NODE_ENV = process.env.NODE_ENV || "development"
+export const API_VERSION = process.env.API_URL || "1"
+export const API_URL =
+  process.env.API_URL || `http://localhost:${PORT}/api/v${API_VERSION}`
